61bfd4429aa337a9fca4d992efec7e2b770d3948
Irio Irineu Musskopf Junior <irio@startocean.com>
2012-04-05 23:47:51 -0300

    remove view because i cant save files in heroku

--- app/controllers/bookshelf_controller.rb
+++ app/controllers/bookshelf_controller.rb
@@ -4,14 +4,16 @@ class BookshelfController < ApplicationController
 
   def show
     if params[:id]
+
       unless params[:format] == "csv"
         redirect_to controller: "bookshelf", action: "show", format: "csv"
         return false
       end
+
       bookshelf = Bookshelf.new(params[:id])
 
       bookshelf.fetch do |books_count|
-        puts "=#{books_count}"
+        puts "books_count=#{books_count}"
       end
       
       response.headers["Content-Type"] = 'text/csv'
--- app/models/bookshelf.rb
+++ app/models/bookshelf.rb
@@ -45,7 +45,6 @@ class Bookshelf
         
         book_bookshelf_id = book_node.get_attribute("id")
         books << Book.new(book_bookshelf_id)
-        puts "Skoob :: read_bookshelf(#{@user_id}) :: page=#{page} book=#{@books.length + books.length}"
         yield books.length if block_given?
 
         break if @options.include?(:just_first_book)
